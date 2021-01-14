3afee007094c1253f289e07c1ca289dfcbbe96ae
Resky Ramadhandi S <reskyramadhandi@student.upi.edu>
2021-01-14 16:45:23 +0700

    Create app
    Learn how to create new app in django.
    #06

--- myFirstDjango/urls.py
+++ myFirstDjango/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from blog import views as blogViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
     path('about/', views.about),
+    path('blog/', blogViews.index),
 ]
